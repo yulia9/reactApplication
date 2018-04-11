87c15b06c14cb5390f2d79fa606dc1a922a02f0e
Yulia Samuilik <usamuilik@gmail.com>
2018-04-12 00:19:23 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,6 @@
+let React = require('react');
+//let ReactDOM = require('react-dom'); 
+//import registerServiceWorker from './registerServiceWorker';
+console.log(20)
+//ReactDOM.render(<App />, document.getElementById('app'));
+//registerServiceWorker();
---
+++ server.js
@@ -0,0 +1,8 @@
+let express = require('express'); 
+let app = express();
+
+app.use(express.static(__dirname));
+
+app.listen(8080, function() {
+	console.log('server started');
+})
