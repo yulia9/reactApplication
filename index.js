e9d214545e7a563374aa1e7aebbe16aa52606030
Yulia Samuilik <usamuilik@gmail.com>
2018-04-12 23:56:10 +0300

    add components

--- index.js
+++ index.js
@@ -1,6 +1,74 @@
-let React = require('react');
-//let ReactDOM = require('react-dom'); 
-//import registerServiceWorker from './registerServiceWorker';
-console.log(20)
-//ReactDOM.render(<App />, document.getElementById('app'));
-//registerServiceWorker();
+let AppParams = {
+  title: 'netflixroulettes'
+},
+HeaderParams = {
+  searchTitle: 'FIND YOUR MOVIE',
+},
+SearchParams = {
+  defaultInputVal: 'Want to watch...',
+  urlSearchByTitle: 'http://react-cdp-api.herokuapp.com/movies?search=',
+}
+
+let Title = React.createElement(
+  "h2",
+  null,
+  AppParams.title
+);
+
+
+function Header(props) {
+  return (
+    <header>
+      {Title}
+      <Search searchTitle={HeaderParams.searchTitle}/>
+    </header>
+  )
+}
+
+class Search extends React.Component{
+	constructor(props) {
+    super(props);
+    this.state = {inputVal: ''};
+    this.updateInputVal = this.updateInputVal.bind(this);
+    this.startSearch = this.startSearch.bind(this);
+  }
+
+  updateInputVal(e) {
+    this.setState({inputVal: e.target.value});
+  }
+  startSearch(e) {
+	  console.log('this.state.inputVal', this.state.inputVal);
+	  e.preventDefault();
+    this.setState({inputVal: ''});
+
+    fetch(SearchParams.urlSearchByTitle + this.state.inputVal).
+      then(function(response) {
+        return response.json();
+    }).then(function(response) {
+      console.log('2', response.data)
+    })
+  }
+  render() {
+    return (
+      <div>
+        <h4> {this.props.searchTitle} </h4>
+        <form onSubmit={this.startSearch}>
+          <input type="text" value={this.state.inputVal} onChange={this.updateInputVal} placeholder={SearchParams.defaultInputVal}/>
+          <button onClick={this.startSearch}> SEARCH </button>
+        </form>
+      </div>
+    );
+  }
+}
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <Header/>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<App/>, document.getElementById('app'));
